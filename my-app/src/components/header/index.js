ca3fe1abd1899623187bcc22fb34955fc5b9079d
Muhammad Danial <73935969+danialkhan0090@users.noreply.github.com>
2025-02-23 15:28:57 +0500

    sweeping

--- my-app/src/components/header/index.js
+++ my-app/src/components/header/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import {
   AppBar,
@@ -8,26 +8,115 @@ import {
   Box,
   IconButton,
   Badge,
+  Menu,
+  MenuItem,
+  InputBase,
+  alpha,
+  styled,
 } from "@mui/material";
 import {
   Person,
   ShoppingCart,
-  Search,
+  Search as SearchIcon,
   Menu as MenuIcon,
+  AccountCircle,
+  Settings,
+  ExitToApp,
 } from "@mui/icons-material";
+
+// Styled search component
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.black, 0.05),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.black, 0.1),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
 
 const Header = () => {
   const navigate = useNavigate();
   const user = JSON.parse(localStorage.getItem("user"));
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [mobileMenuAnchor, setMobileMenuAnchor] = useState(null);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMenuAnchor(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    setMobileMenuAnchor(null);
+  };
 
   const handleLogout = () => {
     localStorage.removeItem("user");
+    handleMenuClose();
     navigate("/login");
   };
 
+  const handleProfile = () => {
+    handleMenuClose();
+    navigate("/profile");
+  };
+
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      open={Boolean(anchorEl)}
+      onClose={handleMenuClose}
+      transformOrigin={{ horizontal: "right", vertical: "top" }}
+      anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+    >
+      <MenuItem onClick={handleProfile}>
+        <AccountCircle sx={{ mr: 2 }} /> Profile
+      </MenuItem>
+      <MenuItem onClick={handleMenuClose}>
+        <Settings sx={{ mr: 2 }} /> Settings
+      </MenuItem>
+      <MenuItem onClick={handleLogout}>
+        <ExitToApp sx={{ mr: 2 }} /> Logout
+      </MenuItem>
+    </Menu>
+  );
+
   return (
     <AppBar
-      position="static"
+      position="sticky"
       sx={{ backgroundColor: "white", color: "#1C1C1C" }}
     >
       <Toolbar sx={{ justifyContent: "space-between" }}>
@@ -38,7 +127,8 @@ const Header = () => {
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2 }}
+            onClick={handleMobileMenuOpen}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
             <MenuIcon />
           </IconButton>
@@ -58,29 +148,35 @@ const Header = () => {
 
         {/* Center Section */}
         <Box display="flex" alignItems="center" flex={1} mx={2}>
-          <Button component={Link} to="/" sx={{ color: "inherit", mx: 1 }}>
-            Home
-          </Button>
-          <Button
-            component={Link}
-            to="/ProductPage"
-            sx={{ color: "inherit", mx: 1 }}
-          >
-            Products
-          </Button>
+          <Box sx={{ display: { xs: "none", sm: "flex" } }}>
+            <Button component={Link} to="/" sx={{ color: "inherit", mx: 1 }}>
+              Home
+            </Button>
+            <Button
+              component={Link}
+              to="/ProductPage"
+              sx={{ color: "inherit", mx: 1 }}
+            >
+              Products
+            </Button>
+          </Box>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
         </Box>
 
         {/* Right Section */}
         <Box display="flex" alignItems="center">
-          <IconButton color="inherit">
-            <Search />
-          </IconButton>
-
           {user ? (
             <>
               <Button
-                component={Link}
-                to="/profile"
+                onClick={handleProfileMenuOpen}
                 startIcon={<Person />}
                 sx={{
                   color: "inherit",
@@ -113,6 +209,7 @@ const Header = () => {
           )}
         </Box>
       </Toolbar>
+      {renderMenu}
     </AppBar>
   );
 };
